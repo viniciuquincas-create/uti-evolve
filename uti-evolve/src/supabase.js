c7575daaac92ac5232879cbbe6b8999e6a777eb6
viniciuquincas-create <viniciuquincas@gmail.com>
2026-04-23 20:58:56 -0300

    Delete uti-evolve directory

--- uti-evolve/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://scuqankwjemqmtjwgema.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjdXFhbmt3amVtcW10andnZW1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NzIxMDksImV4cCI6MjA5MjU0ODEwOX0.YFK_1s5fed12sLMZRB4kXct6_DDsxAheB5hWyKlcUIg'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
