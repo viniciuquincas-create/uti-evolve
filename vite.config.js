f4c351734306c8e29f3a3251a40247031b658e5f
viniciuquincas-create <viniciuquincas@gmail.com>
2026-05-10 18:17:15 -0300

    fix: inject BUILD_TS 2026-05-10T21:16

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.BUILD_TS': JSON.stringify('2026-05-10T21:16'),
+  },
   build: {
-    rollupOptions: {
-      cache: false,
-    },
     chunkSizeWarningLimit: 1000,
   },
 })
