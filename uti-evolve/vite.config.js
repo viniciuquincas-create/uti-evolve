28bfdfa1e3a43bd91c44101f9f6e152b18d9725e
viniciuquincas-create <viniciuquincas@gmail.com>
2026-06-04 17:45:59 -0300

    fix: disable minification to prevent App truncation

--- uti-evolve/vite.config.js
+++ uti-evolve/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 1000,
+    minify: false,
+    chunkSizeWarningLimit: 2000,
   },
 })
